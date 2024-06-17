89e3348f1f17cf58e004c80ed7cebe9eb4b004d4
AquoxHesys <aquoxhesys@gmail.com>
2024-06-17 18:11:54 +0000

    Adjust arrow visibility and add image click event for next image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const images = [
-    { src: "Galeria/d1.png", description: "Diagrama de flujo" },
+    { src: "Galeria/d5.png", description: "Equipo AQUOX" },
     { src: "Galeria/d2.png", description: "Evolución del Proceso de Clarificación del Agua" },
+    { src: "Galeria/d1.png", description: "Diagrama de flujo" },
     { src: "Galeria/d3.png", description: "Esquema Detallado de la Celda de Electrolisis: Vista Frontal, Superior e Isométrica" },
     { src: "Galeria/d4.png", description: "Tarjeta Recolectora" }
 ];
